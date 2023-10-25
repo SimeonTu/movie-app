06fde5bda2b5fe17f16af4e70c24386afd5cf4d9
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2023-10-25 20:23:46 +0100

    updated docs and added placeholder endpoints

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const uuid = require("uuid");
 const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
@@ -40,10 +42,75 @@ app.use(morgan(morganFormatted)); //Log to console
 
 app.use(morgan(morganFormatted, { stream: accessLogStream })); //Log to "/logs/access.log"
 
+app.use(bodyParser.json());
+
 app.use(express.static("public"));
 
 app.get("/movies", (req, res) => {
   res.sendFile(path.join(__dirname + "/movies.json"));
+});
+
+app.get("/movies/:title", (req, res) => {
+  res.send(
+    "A JSON object holding data about a single movie by title (description, genre, director, image URL)"
+  );
+});
+
+app.get("/genres/:name", (req, res) => {
+  res.send(
+    "A JSON object holding data about the name of the genre and a description"
+  );
+});
+
+app.get("/directors/:name", (req, res) => {
+  res.send(
+    "A JSON object holding data about a director's name, bio, birth year, and death year"
+  );
+});
+
+app.post("/users", (req, res) => {
+  let newUser = req.body;
+
+  if (newUser.username && newUser.password && newUser.email) {
+    newUser.id = uuid.v4();
+    res
+      .status(201)
+      .send(
+        "Successfully registered new user: \n" +
+          JSON.stringify(newUser, undefined, 4)
+      );
+  } else {
+    const message = "Missing or wrong parameters in request body";
+    res.status(400).send(message);
+  }
+});
+
+app.put("/users/:username", (req, res) => {
+  let updatedInfo = req.body;
+  res
+    .status(201)
+    .send(
+      "Successfully updated info: \n" +
+        JSON.stringify(updatedInfo, undefined, 4)
+    );
+});
+
+app.post("/users/:username/movies/:movieID", (req, res) => {
+  res.send(
+    "A text message indicating that the movie has been added to the user's list of favorites"
+  );
+});
+
+app.delete("/users/:username/movies/:movieID", (req, res) => {
+  res.send(
+    "A text message indicating that the movie has been removed from the user's list of favorites"
+  );
+});
+
+app.delete("/users/:username", (req, res) => {
+  res.send(
+    "A text message indicating that the user has successfully deregistered their account"
+  );
 });
 
 app.use(function (req, res, next) {
@@ -64,3 +131,5 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080.");
 });
+
+// npm dev run <-- for starting with nodemon
