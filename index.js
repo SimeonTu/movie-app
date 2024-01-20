19f09d7f222acb077634931c72db20f07335f95c
SimeonTu <45809363+SimeonTu@users.noreply.github.com>
2024-01-20 22:18:36 +0000

    CORS policy Netlify update

--- index.js
+++ index.js
@@ -60,10 +60,28 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 let auth = require("./auth")(app); //Endpoint for logging in as a registered user / located in auth.js
+
 require("./passport");
 const passport = require("passport");
+
 const cors = require("cors");
-app.use(cors());
+let allowedOrigins = ["https://ifdbase.netlify.app", "http://localhost:1234"];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 //1. Return a list of all movies to the user
 app.get(
